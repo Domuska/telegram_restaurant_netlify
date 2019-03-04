a6348db4b6371856cc96068a6c1bd2e564c9175b
Tomi Lämsä <lamsatom@gmail.com>
2019-03-04 21:36:01 +0200

    another update, testing telegram API

--- express/server.js
+++ express/server.js
@@ -3,16 +3,44 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const router = express.Router();
 
 console.log('starting up!');
 
+const botUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_KEY}/`;
+
+function getChatId(data) {
+    return data.message.chat.id;
+}
+
+function getMessage(data){
+    return data.message.text;
+}
+
+function revertMessage(msg) {
+    return msg.split("").reverse().join("");
+}
+
 router.get('/', (req, res) => {
     console.log('stuff requested');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
-    res.end();
+    const url = `${botUrl}sendMessage?chat_id=${getChatId(req)}&text=${getMessage(req)}`;
+    console.log(url);
+
+    axios.post(url)
+        .then((response) => {
+            console.log('got response:');
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    res.status(200).send();
+    //res.writeHead(200, { 'Content-Type': 'text/html' });
+    //res.write('<h1>Hello from Express.js!</h1>');
+    //res.end();
 });
 
 router.get('/heitomi', (req, res) => {
